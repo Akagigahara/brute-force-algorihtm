843cf0456cc13e520d3ca14933f025d627f4055f
Leon Wichern <leon.wichern@hotmail.com>
2021-01-04 12:10:47 +0100

    Completion

--- src/code/Breaker.java
+++ src/code/Breaker.java
@@ -1,35 +1,76 @@
 package code;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Breaker {
-	public static Object codebreaker(String pwd)
+	
+	public static Object codebreaker(String pwd) throws IOException
 	{
-		String input = pwd;
 		boolean status = false;
-		String str = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890.!?/-_.:,;#'+*´`~ß=<>|@^°";
+		String str = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890.!?/-_#'*+`~=<>|@^";
 		char[] elligable_chars = str.toCharArray();
+		String check = "";
 		String looper = "";
 		
-		//while(!status)
-		//{
-			
-			for (char letter : elligable_chars)
-			{
-				if(looper.isEmpty())
-				{
-					looper = Character.toString(letter);
-						
-				}if (looper.contentEquals(input))
-				{
-					System.out.println("Got it");
-					status = true;
-					break;
-				}
-			}
+		while(!status)
+		{
 			for (char letter : elligable_chars)
 			{
 				
+				check = looper + Character.toString(letter);
+				if (check.contentEquals(pwd))
+				{
+					status = true;
+				}
 			}
-		//}
+			looper = pushString(check);
+		}
+			
 		return null;
 	}
-}
+	
+		public static String pushString(String subject)
+		{
+			char[] change  = subject.toCharArray();
+			char[] temp = change;
+			String str = "°AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890.!?/-_#'*+`~=<>|@^";
+			boolean precededMaxed = false;
+		
+			for(int pos = change.length-1; pos >= 0; pos--)
+			{
+				if(change[pos] == '^' && pos == change.length-1 || precededMaxed)
+				{
+					if(change.length == 1)
+					{
+						change[pos] = 'A';
+					}
+					
+					else if(pos == 0)
+					{
+						temp[pos] = 'A';
+						temp[pos+1] = 'A';
+					}
+					
+					else
+					{
+						switch(change[pos-1])
+						{
+							case '^':
+								precededMaxed = true;
+								temp[pos-1] = 'A';
+								break;
+								
+							default:
+								temp[pos-1] = str.charAt(str.indexOf(change[pos-1])+1);
+								precededMaxed = false;
+								temp = Arrays.copyOfRange(change, 0, change.length-1);
+								break;
+						}
+					}
+				}
+			}
+			String returner = String.valueOf(temp);
+			return returner;
+		}
+	}
--- src/code/Console.java
+++ src/code/Console.java
@@ -12,9 +12,20 @@ public class Console
 		System.out.println("Please insert password");
 		String input = "invalid";
 		input = reader();
-		Breaker.codebreaker(input);
-		write("");
+		if(input.isBlank())
+		{
+			write("Invalid Input");
+		}
 		
+		else
+		{			
+			long startTime = System.currentTimeMillis();
+			Breaker.codebreaker(input);
+			long endTime = System.currentTimeMillis();
+			write("Prozess Abgeschlossen. " + milisecConverter(startTime, endTime));
+		}
+		
+		main(args);
 	}
 	
 	public static String reader() throws IOException
@@ -23,6 +34,18 @@ public class Console
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String read = reader.readLine();
 		return read;
+	}
+	
+	public static String milisecConverter(long x1, long x2)
+	{
+		long preFormat = x2-x1;
+		long Miliseconds = preFormat%1000;
+		long seconds = preFormat/1000%60;
+		long minutes = preFormat/1000/60%60;
+		long hours = preFormat/1000/60/60;
+		
+		String deltaT = "Der Algorithmus benötigte " + hours + " Stunden, " + minutes + " Minuten, " + seconds + " Sekunden und " + Miliseconds + " Millisekunden.";
+		return deltaT;
 	}
 	
 	public static void write(String x)
